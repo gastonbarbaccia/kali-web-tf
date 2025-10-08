ca8a896d2c4e5ece1c21721ea9f6553eb6d9321f
Gaston Barbaccia <gastonbarbaccia@devcodeacademy.com>
2025-10-08 01:05:55 -0300

    ok

--- main.tf
+++ main.tf
@@ -63,6 +63,11 @@ resource "aws_instance" "ec2" {
   security_groups             = [aws_security_group.open_all.name]
   associate_public_ip_address = true
 
+  root_block_device {
+    volume_size = 30         # Cambiá este valor según lo que necesites (GB)
+    volume_type = "gp3"      # O "gp2", "io1", etc.
+  }
+
   tags = {
     Name = var.instance_name
   }
